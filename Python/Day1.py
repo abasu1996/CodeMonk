8b66d813b696dd3635e374fc66afe2274c23f34e
Anim Basu <92218437+abasu1996@users.noreply.github.com>
2022-11-19 05:04:36 +0530

    Changes on 19-11-2022

---
+++ Python/Day1.py
@@ -0,0 +1,27 @@
+score = 10
+name = "Anim"
+winning = True
+
+print(f"The Guy {name} scored {score} in the Exam and it is called {winning} winning")
+##PEMDASLR - Paranthesis, Exponentian, Multiplication, Division, Addition, Substraction, Left to Right
+number1 = 13
+number2 = 14
+division = number1 / number2
+print(division)
+final_print = "{:.2f}".format(division)##Syntax for decimal point formatting
+print(final_print)
+##If/Else Statment, Logical Operators and many more
+
+height = int(input("Please Enter your height in CM"))
+if height>=120:
+    print("You can Ride the Roller Coaster")
+else:
+    print("You can't Ride the Roller Coaster")
+
+##Odd or Even Excercise
+number3 = int(input("Please Enter a Number"))
+if (number3%2)==0:
+    print("This is an Even Number")
+else:
+    print("This is an Odd Number")
+
