aa47916c52e9cbb1e209a1adbfa41d838826d328
Anim Basu <92218437+abasu1996@users.noreply.github.com>
2022-12-21 23:38:37 +0530

    Changes on 21-12-2022

--- Python/Python_loops.py
+++ Python/Python_loops.py
@@ -1,3 +1,4 @@
+
 heights = input("Mention the heights of the students...").split(" ")
 total_height = 0
 for item in heights:
@@ -9,4 +10,4 @@ for item2 in heights:
     number_of_strudents+=1
 average_height = total_height / number_of_strudents    
 
-print(f"Average height of the students is{average_height} ")
+print(f"Average height of the students is {average_height} ")
